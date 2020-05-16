c78461477628b8ea61636285daa4a195dd0ca9e0
tascan <ds.arxont@gmail.com>
2020-05-16 15:43:03 +0300

    render function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,49 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data: () => ({
+    name: 'Деном',
+    level: '1'
+  }),
+  render(createElement) {
+    return createElement('div', {
+      attrs: {
+        class: 'box'
+      }
+    }, [
+      createElement(`h${this.level}`, {
+        attrs: {
+          class: 'box-title'
+        }
+      }, 'Добро пожаловать!'),
+      createElement('span', {
+        attrs: {
+          class: 'box-text'
+        },
+      }, `Данная страница создана ${this.name}`)
+    ])
   }
 }
 </script>
+
+
+<style lang="less" scoped>
+  .box {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 400px;
+    height: 200px;
+    margin: 10% auto;
+    background: #c5c9c7;
+    .box-title {
+      color: red;
+    }
+    .box-text {
+      color: blue;
+    }
+  }
+</style>
